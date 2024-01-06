44ab75bf6ac0eef958194d02726e257915ebacca
Cals Ranna <cals@Calss-MBP.lan>
2024-01-06 17:38:04 +0800

    feat(widget): add rhombus container

--- lib/component/toolbar.dart
+++ lib/component/toolbar.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:project_soaring/router/router.dart';
 
@@ -14,13 +12,9 @@ class Toolbar extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         for (var page in pages)
-          Transform.scale(
-            scale: page == '战斗' ? 1.618 : 1,
-            child: ToolbarTile(
-              label: page,
-              shape: page == '战斗' ? BoxShape.circle : BoxShape.rectangle,
-              onTap: () => replace(context, page),
-            ),
+          ToolbarTile(
+            label: page,
+            onTap: () => replace(context, page),
           ),
       ],
     );
@@ -34,7 +28,7 @@ class Toolbar extends StatelessWidget {
       '背包' => const BackpackPageRoute().push(context),
       '地图' => const AreasPageRoute().push(context),
       '地宫' => const DungeonsPageRoute().push(context),
-      '无尽试炼' => const TrialsPageRoute().push(context),
+      '试炼' => const TrialsPageRoute().push(context),
       _ => null,
     };
   }
@@ -44,12 +38,10 @@ class ToolbarTile extends StatefulWidget {
   const ToolbarTile({
     super.key,
     required this.label,
-    this.shape = BoxShape.rectangle,
     this.onTap,
   });
 
   final String label;
-  final BoxShape shape;
   final void Function()? onTap;
 
   @override
@@ -59,35 +51,83 @@ class ToolbarTile extends StatefulWidget {
 class _ToolbarTileState extends State<ToolbarTile> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: widget.onTap,
-      child: SizedBox(
-        height: 88,
-        width: 88,
-        child: Stack(
-          children: [
-            Center(
-              child: Transform.rotate(
-                angle: pi / 4,
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
-                    shape: widget.shape,
-                  ),
-                  height: 48,
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  width: 48,
-                ),
-              ),
-            ),
-            Center(child: Text(widget.label))
-          ],
-        ),
-      ),
+    // return GestureDetector(
+    //   behavior: HitTestBehavior.opaque,
+    //   onTap: widget.onTap,
+    //   child: SizedBox(
+    //     height: 80,
+    //     width: 80,
+    //     child: Stack(
+    //       children: [
+    //         Center(
+    //           child: Transform.rotate(
+    //             angle: pi / 4,
+    //             child: Container(
+    //               alignment: Alignment.center,
+    //               decoration: BoxDecoration(
+    //                 border: Border.all(
+    //                   color: Theme.of(context).colorScheme.onSurface,
+    //                 ),
+    //               ),
+    //               height: 48,
+    //               padding: const EdgeInsets.symmetric(vertical: 8),
+    //               width: 48,
+    //             ),
+    //           ),
+    //         ),
+    //         Center(child: Text(widget.label))
+    //       ],
+    //     ),
+    //   ),
+    // );
+    return RhombusContainer();
+  }
+}
+
+class RhombusContainer extends StatelessWidget {
+  const RhombusContainer({super.key, this.size, this.child});
+
+  final Size? size;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: const _RhombusContainerPainter(),
+      size: size ?? const Size.square(80),
+      child: child,
     );
+  }
+}
+
+class _RhombusContainerPainter extends CustomPainter {
+  const _RhombusContainerPainter();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.white;
+    paint.strokeWidth = 1;
+    paint.style = PaintingStyle.stroke;
+    final path = Path();
+    path.moveTo(size.width / 2, 0);
+    path.lineTo(size.width, size.height / 2);
+    path.lineTo(size.width / 2, size.height);
+    path.lineTo(0, size.height / 2);
+    path.close();
+    canvas.drawPath(path, paint);
+    final fillPath = Path();
+    fillPath.moveTo(size.width / 2, 1);
+    fillPath.lineTo(size.width - 1, size.height / 2);
+    fillPath.lineTo(size.width / 2, size.height - 1);
+    fillPath.lineTo(1, size.height / 2);
+    paint.style = PaintingStyle.fill;
+    paint.color = Colors.blue;
+    canvas.drawPath(fillPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
